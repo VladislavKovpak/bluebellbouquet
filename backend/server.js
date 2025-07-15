6dd94bcbb0832d0221859df88ff0e1abf002a3c4
Vladislav Kovpak <vladislavkovpak1@gmail.com>
2025-07-15 10:11:15 -0700

    update

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(cors({
   ],
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization'],
-  credentials: true
+  credentials: false
 }))
 
 app.options('*', cors())
