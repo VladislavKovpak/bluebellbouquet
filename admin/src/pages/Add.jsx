abcd192b161dbd744c024b652ef01a753980d39a
Vladislav Kovpak <vladislavkovpak1@gmail.com>
2025-07-15 09:48:36 -0700

    update

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -6,10 +6,10 @@ import { toast } from 'react-toastify';
 
 const Add = ({token}) => {
 
-    const [image1, setImage1] = useState(false);
-    const [image2, setImage2] = useState(false);
-    const [image3, setImage3] = useState(false);
-    const [image4, setImage4] = useState(false);
+    //const [image1, setImage1] = useState(false);
+    //const [image2, setImage2] = useState(false);
+    //const [image3, setImage3] = useState(false);
+   // const [image4, setImage4] = useState(false);
 
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
@@ -29,10 +29,10 @@ const Add = ({token}) => {
             formData.append("category", category)
             formData.append("bestseller", bestseller)
 
-            image1 && formData.append("image1", image1)
-            image2 && formData.append("image2", image2)
-            image3 && formData.append("image3", image3)
-            image4 && formData.append("image4", image4)
+           // image1 && formData.append("image1", image1)
+           // image2 && formData.append("image2", image2)
+           // image3 && formData.append("image3", image3)
+           // image4 && formData.append("image4", image4)
 
             const response = await axios.post(backendUrl+"/api/product/add", formData, {headers:{token}})
             
@@ -40,10 +40,10 @@ const Add = ({token}) => {
                 toast.success(response.data.message)
                 setName('')
                 setDescription('')
-                setImage1(false)
-                setImage2(false)
-                setImage3(false)
-                setImage4(false)
+                //setImage1(false)
+               // setImage2(false)
+                //setImage3(false)
+               // setImage4(false)
                 setPrice('')
             } else{
                 toast.error(response.data.message);
@@ -60,24 +60,7 @@ const Add = ({token}) => {
             <div>
                 <p className='mb-2'>Upload Image</p>
             </div>
-            <div className='flex gap-2'>
-                <label htmlFor="image1">
-                    <img className='w-20' src={!image1 ? assets.upload_area : URL.createObjectURL(image1)} />
-                    <input onChange={(e)=>setImage1(e.target.files[0])} type="file" id="image1" hidden />
-                </label>
-                <label htmlFor="image2">
-                    <img className='w-20' src={!image2 ? assets.upload_area : URL.createObjectURL(image2)}/>
-                    <input onChange={(e)=>setImage2(e.target.files[0])} type="file" id="image2" hidden />
-                </label>
-                <label htmlFor="image3">
-                    <img className='w-20' src={!image3 ? assets.upload_area : URL.createObjectURL(image3)} />
-                    <input onChange={(e)=>setImage3(e.target.files[0])} type="file" id="image3" hidden />
-                </label>
-                <label htmlFor="image4">
-                    <img className='w-20' src={!image4 ? assets.upload_area : URL.createObjectURL(image4)}/>
-                    <input onChange={(e)=>setImage4(e.target.files[0])}  type="file" id="image4" hidden />
-                </label>
-            </div>
+            
 
             <div className='w-full'>
                 <p className='mb-2'>Product name</p>
